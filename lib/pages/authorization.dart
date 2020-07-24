89718acf914cf31beb5bb6fece6d3b857804adb3
lokodav <15734Dan!!>
2020-07-24 15:35:00 +0400

    Log in page

---
+++ lib/pages/authorization.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+
+class AuthorizationPage extends StatefulWidget {
+  @override
+  _AuthorizationPageState createState() => _AuthorizationPageState();
+}
+
+class _AuthorizationPageState extends State<AuthorizationPage> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  String _email;
+  String _password;
+  bool showLogin = true;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget _logotype() {
+      return Padding(
+        padding: EdgeInsets.only(top: 100),
+        child: Container(
+          child: Align(
+            child: Text(
+              'WORKOUTS',
+              style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          ),
+        ),
+      );
+    }
+
+    Widget _input(Icon icon, String hint, TextEditingController controller,
+        bool obscure) {
+      return Container(
+        padding: EdgeInsets.only(left: 20, right: 20),
+        child: TextField(
+            controller: controller,
+            obscureText: obscure,
+            style: TextStyle(fontSize: 20, color: Colors.white),
+            decoration: InputDecoration(
+                hintStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.white38),
+                hintText: hint,
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width: 3)),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white60, width: 1)),
+                prefixIcon: Padding(
+                  padding: EdgeInsets.only(left: 10, right: 10),
+                  child: IconTheme(
+                    data: IconThemeData(color: Colors.white),
+                    child: icon,
+                  ),
+                ))),
+      );
+    }
+
+    Widget _button(String text, void func()) {
+      return RaisedButton(
+        splashColor: Theme.of(context).primaryColor,
+        highlightColor: Theme.of(context).primaryColor,
+        color: Colors.white,
+        child: Text(text,
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).primaryColor,
+                fontSize: 20)),
+        onPressed: () {
+          func();
+        },
+      );
+    }
+
+    Widget _form(String label, void func()) {
+      return Container(
+          child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(bottom: 20, top: 10),
+            child: _input(Icon(Icons.email), "EMAIL", _emailController, false),
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 20),
+            child:
+                _input(Icon(Icons.lock), "PASSWORD", _passwordController, true),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: Container(
+              height: 50,
+              width: MediaQuery.of(context).size.width,
+              child: _button(label, func),
+            ),
+          )
+        ],
+      ));
+    }
+
+    void _buttonLogin() {
+      _email = _emailController.text;
+      _password = _passwordController.text;
+      _emailController.clear();
+      _passwordController.clear();
+    }
+
+    return Scaffold(
+        backgroundColor: Theme.of(context).primaryColor,
+        body: Column(
+          children: <Widget>[
+            _logotype(),
+            SizedBox(height: 50),
+            (showLogin
+                ? Column(
+                    children: <Widget>[
+                      _form('Log in', _buttonLogin),
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: GestureDetector(
+                            child: Text(
+                              'Not registered yet?',
+                              style:
+                                  TextStyle(fontSize: 20, color: Colors.white),
+                            ),
+                            onTap: () {
+                              setState(() {
+                                showLogin = false;
+                              });
+                            }),
+                      )
+                    ],
+                  )
+                : Column(
+                    children: <Widget>[
+                      _form('Registration', _buttonLogin),
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: GestureDetector(
+                            child: Text(
+                              'Already registered?',
+                              style:
+                                  TextStyle(fontSize: 20, color: Colors.white),
+                            ),
+                            onTap: () {
+                              setState(() {
+                                showLogin = true;
+                              });
+                            }),
+                      )
+                    ],
+                  )),
+          ],
+        ));
+  }
+}
